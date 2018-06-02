c565919978f737111fb81d85fa492ee3070cb8d8
vivekkadu <kaduvivek8@gmail.com>
2018-06-02 09:36:03 +0530

    original

--- server.js
+++ server.js
@@ -7,22 +7,22 @@ const port = process.env.PORT || 3000;
 var app = express();
 
 
-app.set('View Engine', 'hbs');
+// app.set('View Engine', 'hbs');
 
-//registr midddleware to tell exprees done any work we want
-//we have use to tell program to go next other wise it will not exe another lines of program
+// //registr midddleware to tell exprees done any work we want
+// //we have use to tell program to go next other wise it will not exe another lines of program
 
-app.use((req, res, next) => {
-    var now = new Date().toString();
-    var log = `${now}: ${req.method} ${req.url}`;
+// app.use((req, res, next) => {
+//     var now = new Date().toString();
+//     var log = `${now}: ${req.method} ${req.url}`;
   
-    console.log(log);
-    fs.appendFile('server.log', log + '\n',(err) => {
-      if(err){
-       console.log('error')
-      }});
-    next();
-  });
+//     console.log(log);
+//     fs.appendFile('server.log', log + '\n',(err) => {
+//       if(err){
+//        console.log('error')
+//       }});
+//     next();
+//   });
   
 //   app.use((req, res, next ) => {
 //       res.render('maintenance.hbs')
@@ -30,18 +30,18 @@ app.use((req, res, next) => {
 
 
 // Partials are reusble componets that are used in handlebars
-hbs.registerPartials(__dirname + '/views/partials');
+// hbs.registerPartials(__dirname + '/views/partials');
 
 
-hbs.registerHelper('getCurrentYear', () => {
-    return new Date().getFullYear()
-});
+// hbs.registerHelper('getCurrentYear', () => {
+//     return new Date().getFullYear()
+// });
 
 
 
-hbs.registerHelper('streamIt', (text) => {
-    return text.toUpperCase();
-});
+// hbs.registerHelper('streamIt', (text) => {
+//     return text.toUpperCase();
+// });
 
 
 
@@ -49,13 +49,13 @@ hbs.registerHelper('streamIt', (text) => {
 //use for using staic pages which return simple html page 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/',(req, res) => {
+// app.get('/',(req, res) => {
 
-     res.render('home.hbs', {
-         welcomeMessage: 'Welcome to Home Page',
-         pageTitle: 'Home Page'
-     })
-});
+//      res.render('home.hbs', {
+//          welcomeMessage: 'Welcome to Home Page',
+//          pageTitle: 'Home Page'
+//      })
+// });
 
 // app.get('/', (req, res) => {
 //  // res.send('<H1>Hello World</H1>');
@@ -68,26 +68,26 @@ app.get('/',(req, res) => {
 //   })
 // });
 
-app.get('/about', (req, res) => {
-   res.render('about.hbs',{
-       pageTitle: 'AboutPage',
-   })
-});
+// app.get('/about', (req, res) => {
+//    res.render('about.hbs',{
+//        pageTitle: 'AboutPage',
+//    })
+// });
 
 
-app.get('/justPage', (req, res) => {
-    res.render('justPage.hbs',{
-        pageTitle: 'JustPage',
-    })
- });
+// app.get('/justPage', (req, res) => {
+//     res.render('justPage.hbs',{
+//         pageTitle: 'JustPage',
+//     })
+//  });
  
 
 
-app.get('/bad', (req, res) => {
-  res.send({
-      error: "Unable to connect"
-    });
-});
+// app.get('/bad', (req, res) => {
+//   res.send({
+//       error: "Unable to connect"
+//     });
+// });
 
 
 app.listen(port, () => {
